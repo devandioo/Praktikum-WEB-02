cf6cacecbe6dac8f4682a97c12bcabe19cc8833b
Devandio Naufal Zuhair <92972902+devandioo@users.noreply.github.com>
2022-04-04 01:40:50 -0700

    Add files via upload

---
+++ Tugas_Praktikum03/startbootstrap-sb-admin-gh-pages/view/Cartikel1.php
@@ -0,0 +1,231 @@
+<?php
+include '../layout/head_er.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+   
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="carousel slide" id="carousel-465534">
+				<ol class="carousel-indicators">
+					<li data-slide-to="0" data-target="#carousel-465534">
+					</li>
+					<li data-slide-to="1" data-target="#carousel-465534">
+					</li>
+					<li data-slide-to="2" data-target="#carousel-465534" class="active">
+					</li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="carousel-item">
+						<img class="d-block w-100" alt="Carousel Bootstrap First" src="../fonts/rusVSUk.jpg">
+						<div class="carousel-caption">
+							<h4>
+								SEMAKIN MEMANAS! Rusia Kencangkan serangan!
+							</h4>
+							<p>
+								serangan rusia kepada ukraina mengundang serangan2 Dari negara anggota NATO......
+							</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img class="d-block w-100" alt="Carousel Bootstrap Second" src="../fonts/tank.jpg">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+
+								</p>
+						</div>
+					</div>
+					<div class="carousel-item active">
+						<img class="d-block w-100" alt="Carousel Bootstrap Third" src="../fonts/tentara.jpg">
+						<div class="carousel-caption">
+							<h4>
+								
+							</h4>
+							<p>
+								
+							</p>
+						</div>
+					</div>
+				</div> <a class="carousel-control-prev" href="#carousel-465534" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-465534" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8">
+			<div class="jumbotron">
+				<h2>
+					LAutan mayat di Kota- kota Ukraina
+				</h2>
+				<p>
+                Jakarta, CNBC Indonesia - Perang dunia maya yang terkait dengan konflik Ukraina-Rusia melonjak <br>
+					 ketika sukarelawan digital dari seluruh dunia memasuki arena 'pertarungan'<br>
+Jumlah serangan siber yang dilakukan oleh - dan atas nama - kedua negara sejak pecahnya <br>
+perang cukup mengejutkan. <br> <br>
+
+Menurut badan penelitian Check Point Software Technologies untuk pertama kalinya dalam sejarah, siapa pun orangnya dapat bergabung dalam perang. <br>
+"Kami melihat seluruh komunitas dunia maya terlibat, di mana banyak kelompok dan individu telah memihak, baik Rusia atau Ukraina. <br> Terjadi banyak kekacauan dunia maya," kata Lotem Finkelstein, kepala intelijen ancaman di Check Point Software, dikutip dari CNBC Indonesia, Senin (14/3/2022). <br>
+Dalam tiga hari pertama setelah serangan, serangan online terhadap militer Ukraina dan sektor pemerintah  <br> meningkat 196%, menurut Check Point Research (CPR). Menurut data, serangan juga sedikit meningkat terhadap organisasi Rusia (4%) dan Ukraina (0,2%). <br>
+
+Sejak itu, pihak berwenang Ukraina memperkirakan ada sekitar 400.000 peretas multinasional telah secara sukarela membantu Ukraina, <br>
+
+"Relawan akar rumput menciptakan gangguan yang meluas - membuat coretan pesan anti-perang di media Rusia dan membocorkan <br> data dari operasi peretasan saingannya," ungkap Yuval Wollman, presiden perusahaan keamanan siber CyberProof dan mantan direktur <br> jenderal Kementerian Intelijen Israel.
+
+"Belum pernah kami melihat tingkat keterlibatan aktor luar yang tidak terkait dengan konflik ini." tuturnya. <br>
+
+Menurut data CPR tiga minggu kemudian, Ukraina terus mendapatkan rentetan serangan online, dengan sebagian besar <br> ditujukan pada pemerintah dan militernya.
+
+Moskow sendiri secara konsisten membantah terlibat dalam perang <br> siber atau membantu serangan siber. Pada 19 Februari, kedutaan Rusia di Washington mengatakan di Twitter bahwa mereka tidak pernah melakukan dan tidak melakukan operasi 'jahat' di dunia maya. <br>
+
+Data CPR menunjukkan serangan terhadap Rusia menurun selama jangka waktu yang <br> sama. Ada beberapa alasan untuk itu, termasuk upaya Rusia mengurangi visibilitas serangan atau meningkatkan keamanan <br> untuk mempertahankan diri dari serangan tersebut.
+
+Tentara TI Ukraina
+
+Sebagai target lama dari dugaan serangan siber Rusia, Ukraina tampaknya menyambut bantuan digital secara global. <br>
+
+Menyusul permintaan yang diposting di Twitter oleh menteri digital Ukraina Mykhailo Fedorov, sekitar 308.000 orang bergabung dengan grup Telegram yang dikenal <br> sebagai "Tentara TI Ukraina."
+
+Salah satu anggota kelompok tersebut adalah Gennady Galanter, salah satu pendiri perusahaan teknologi informasi Provectus. Dia mengatakan <br> kelompok itu fokus pada mengganggu situs-situs Rusia, mencegah disinformasi dan mendapatkan informasi yang akurat kepada warga Rusia. <br>
+	
+							</p>
+				
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="list-group">
+				 <a href="#" class="list-group-item list-group-item-action active">TOP NEWS	</a>
+				<div class="list-group-item">
+					Vaksin Booster buat Apa lagi?
+				</div>
+				<div class="list-group-item">
+					<p class="list-group-item-text">
+						F-35 Rusia ikut membombardir ukraina kalang kabut
+					</p>
+				</div>
+				<div class="list-group-item justify-content-between">
+					Jokowi 3 Periode? <span class="badge badge-secondary badge-pill">14</span>
+				</div> <a href="#" class="list-group-item list-group-item-action active justify-content-between">Berita lain <span class="badge badge-light badge-pill">14</span></a>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail First" src="../fonts/hacker-hacking-background-hd-1080p-457507.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+								hello virus
+							</h5>
+							<p class="card-text">
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Buka</a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail Second" src="../fonts/naonimus.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+								WHo Am i
+							</h5>
+							<p class="card-text">
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Buka</a> 
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail Third" src="../fonts/hckr tpg.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+								Hacker Berulah
+							</h5>
+							<p class="card-text">
+								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Buka	</a> 
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<fieldset>
+			<form role="form">
+				<div class="form-group">
+					 <h3 style="text-align: center;">Login Ke Akun Anda</h3> <hr>
+					<label for="exampleInputEmail1">
+						Email address
+					</label>
+					<input type="email" class="form-control" id="exampleInputEmail1">
+				</div>
+				<div class="form-group">
+					 
+					<label for="exampleInputPassword1">
+						Password
+					</label>
+					<input type="password" class="form-control" id="exampleInputPassword1">
+				</div>
+				<div class="form-group">
+					 
+					<label for="exampleInputFile">
+						
+					</label>
+					<input type="file" class="form-control-file" id="exampleInputFile">
+					<p class="help-block">
+						Lupa pasword?
+					</p>
+				</div>
+				<div class="checkbox">
+					 
+			
+				</div> 
+				<button type="submit" class="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</fieldset>
+		</div>
+	</div>
+</div>
+
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/scripts.js"></script>
+  </body>
+</html>
+
+<?php
+include '../layout/footer.php';
+?>
